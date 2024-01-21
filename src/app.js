324434eb4b199dbc7112327914e4356baddc42b9
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2024-01-21 16:13:13 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,5 +10,12 @@ app.use(cors({
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}));
 app.use(express.static('public'));
-app.use(cookieParser())
+app.use(cookieParser());
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+//routes decleration
+app.use('/api/v1/users',userRouter)
+
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import dotenv from 'dotenv'
-import express from "express";
+
 import connectDB from "./db/index.js";
-const app = express();
+import {app} from './app.js'
+
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT ||3000,()=>{
-        console.log(`App is listening on port${process.env.PORT||3000}`);
+    app.listen(process.env.PORT ||5000,()=>{
+        console.log(`App is listening on port${process.env.PORT }`);
     })
 })
 .catch((err)=>{
